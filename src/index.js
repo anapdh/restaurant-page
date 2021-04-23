503695bc9a9cb60f995ec7aea852130b096d43f2
anapdh <ana_d_hubner@hotmail.com>
2021-04-23 18:18:10 -0300

    Add home js and loader for css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello Webpack!");
+import '../assets/styles/style.css';
+// import 'bootstrap';
+import Home from './home';
+
+const content = document.getElementById('content');
+const home = Home();
+content.appendChild(home);
+
+// function navbar() {
+//   const element = document.createElement('div');
+//     element.innerHTML = 'Menu'
+//   const
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
 };
