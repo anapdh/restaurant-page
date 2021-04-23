d94986c0375afe48a8782d58a1853a04ee4c6bc7
anapdh <ana_d_hubner@hotmail.com>
2021-04-23 18:25:21 -0300

    Import bootstrap

--- src/home.js
+++ src/home.js
@@ -1,27 +1,27 @@
 const Home = () => {
 
-  // const homeContent = document.createElement('div');
-  // homeContent.setAttribute('class', 'container mt-5');
+  const homeContent = document.createElement('div');
+  homeContent.setAttribute('class', 'container mt-5');
 
-  // const homeHeader = document.createElement('h1');
-  // homeHeader.setAttribute('class', 'm-2');
-  // homeHeader.textContent = 'My Restaurant';
+  const homeHeader = document.createElement('h1');
+  homeHeader.setAttribute('class', 'm-2');
+  homeHeader.textContent = 'My Restaurant';
 
-  // const homeImage = document.createElement('img');
-  // homeImage.setAttribute('class', 'bg-image');
-  // homeImage.setAttribute('src', '/assets/imgs/pf-br.jpg');
+  const homeImage = document.createElement('img');
+  homeImage.setAttribute('class', 'bg-image');
+  homeImage.setAttribute('src', '/assets/imgs/pf-br.jpg');
 
-  // const homeParagraph = document.createElement('p');
-  // homeParagraph.setAttribute('class', 'm-2');
-  // homeParagraph.textContent = 'CONTENT heree';
+  const homeParagraph = document.createElement('p');
+  homeParagraph.setAttribute('class', 'm-2');
+  homeParagraph.textContent = 'CONTENT heree';
 
 
-  // homeContent.appendChild(homeHeader);
-  // homeContent.appendChild(homeImage);
-  // homeContent.appendChild(homeParagraph);
+  homeContent.appendChild(homeHeader);
+  homeContent.appendChild(homeImage);
+  homeContent.appendChild(homeParagraph);
 
-  // return homeContent;
-
+  return homeContent;
+  
 };
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import '../assets/styles/style.css';
-// import 'bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Home from './home';
 
 const content = document.getElementById('content');
 const home = Home();
 content.appendChild(home);
-
-// function navbar() {
-//   const element = document.createElement('div');
-//     element.innerHTML = 'Menu'
-//   const
-// }
