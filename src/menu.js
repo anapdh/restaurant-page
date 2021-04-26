5c1cc6d05714df254dbca4caef73f6861d5f5ab5
anapdh <ana_d_hubner@hotmail.com>
2021-04-26 16:34:50 -0300

    Add menu items

--- src/menu.js
+++ src/menu.js
@@ -4,25 +4,57 @@ const Menu = () => {
   menuContent.setAttribute('class', 'container');
 
   const menuHeader = document.createElement('h1');
-  menuHeader.setAttribute('class', 'text-center my-3 pt-3');
-  menuHeader.innerHTML = 'My Restaurant';
+  menuHeader.setAttribute('class', 'text-center my-3 pt-3 mb-5');
+  menuHeader.innerHTML = 'MENU';
 
   // const menuImage = document.createElement('img');
   // menuImage.setAttribute('class', 'bg-image');
   // menuImage.setAttribute('src', '/assets/imgs/pf-br.jpg');
 
-  const menuText = document.createElement('h3');
-  menuText.setAttribute('class', 'h3 my-3');
-  menuText.innerHTML = 'Themost famous plates in the world are now ready for you.';
+  const menuList = document.createElement('ul');
+  menuList.setAttribute('class', 'menuList')
 
-  const menuParagraph = document.createElement('p');
-  menuParagraph.setAttribute('class', 'm-3 pb-3');
-  menuParagraph.innerHTML = 'Check out our menu.';
+    const menuItems = () => {
+      const list = document.createElement('li');
+      list.setAttribute('class', 'nav-item list-unstyled mx-2')
+
+        const menu1 = document.createElement('h5');
+        menu1.setAttribute('id', 'menu1')
+        menu1.setAttribute('class', 'mx-2')
+        menu1.innerHTML = 'CHICKEN';
+        list.appendChild(menu1);
+          let menuParagraph1 = document.createElement('p');
+          menuParagraph1.setAttribute('class', 'm-3 pb-3');
+          menuParagraph1.innerHTML = 'Check out our menu.';
+          list.appendChild(menuParagraph1);
+
+        const menu2 = document.createElement('h5');
+        menu2.setAttribute('id', 'menu2')
+        menu2.setAttribute('class', 'mx-2')
+        menu2.innerHTML = 'BARBECUE';
+        list.appendChild(menu2);
+          const menuParagraph2 = document.createElement('p');
+          menuParagraph2.setAttribute('class', 'm-3 pb-3');
+          menuParagraph2.innerHTML = 'Check out our menu.';
+          list.appendChild(menuParagraph2);
+
+        const menu3 = document.createElement('h5');
+        menu3.setAttribute('id', 'menu3')
+        menu3.setAttribute('class', 'mx-2')
+        menu3.innerHTML = 'FISH';
+        list.appendChild(menu3);
+          const menuParagraph3 = document.createElement('p');
+          menuParagraph3.setAttribute('class', 'm-3 pb-3');
+          menuParagraph3.innerHTML = 'Check out our menu.';
+          list.appendChild(menuParagraph3);
+
+        return list;
+    };
 
   menuContent.appendChild(menuHeader);
   // menuContent.appendChild(menuImage);
-  menuContent.appendChild(menuText);
-  menuContent.appendChild(menuParagraph);
+  menuContent.appendChild(menuItems());
+  // menuContent.appendChild(menuParagraph);
 
   return menuContent;
 
